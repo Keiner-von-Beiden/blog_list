e91f80e253729518e607be05a56e323959a9cffe
Sergei Fedorov <s.n.feodorov@gmail.com>
2024-10-26 21:50:01 +0300

    Initial commit

---
+++ models/blogmodel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+//const config = require('../utils/config')
+//const logger = require('./utils/logger')
+
+
+const blogSchema = new mongoose.Schema({
+    author: String,
+    title: String,
+    url: String,
+    likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
